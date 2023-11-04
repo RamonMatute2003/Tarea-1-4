3ee1c2c9a3055f8c9c4cc105b163d6b71aa829bc
Ramon Matute <ramonmatute2003@gmail.com>
2023-11-04 00:23:28 -0600

    Listo todo

--- Views/View_data.xaml.cs
+++ Views/View_data.xaml.cs
@@ -2,10 +2,8 @@ namespace Tarea_1_4.Views;
 
 public partial class View_data : ContentPage
 {
-	public View_data()
-	{
+	public View_data(){
 		InitializeComponent();
-
 	}
 
     protected override async void OnAppearing() {
